dc9150c38f48996aeb302146d437775edabcff9b
samuel Temitope <stemitope370@gmail.com>
2020-02-24 21:07:37 +0100

    added styles to the css

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+let Input = document.querySelector('input[type="text"]');
+
+let ul = document.querySelector('ul');
+let container = document.querySelector('div');
+let lists = document.querySelector('li');
