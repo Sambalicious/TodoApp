300d1cb25159697fa9397edc763330d9dbff5821
samuel Temitope <stemitope370@gmail.com>
2020-02-25 00:44:26 +0100

    more functionality added

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ let spans = document.getElementsByTagName('span');
 let pencil = document.querySelector('#pencil');
 let saveBtn = document.querySelector('.save');
 let clearBtn = document.querySelector('.clear');
-
+let time = document.querySelector('#time');
 
 function deleteTodo(){
     for(let span of spans){
@@ -31,17 +31,19 @@ if(localStorage.getItem('todoList')){
 //listen to event on keypress
 
 Input.addEventListener('keypress', function(e){
+        
     if(e.which===13){
         let li =document.createElement('li');
         let span = document.createElement('span');
         let icon = document.createElement('i');
-
+        let when= time.value;
         let newTodo = this.value;
         this.value = '';
 
         icon.classList.add('fas', 'fa-trash');
-        span.prepend(icon);
-        ul.appendChild(li).prepend(span, newTodo);
+        span.append(icon);
+        ul.appendChild(li).prepend( when, span, newTodo);
+        
 
         deleteTodo();
     }
@@ -59,7 +61,7 @@ ul.addEventListener('click', function(e){
 ///hide input when pencil icon is clicked
 
 pencil.addEventListener('click', function(){
-    input.classList.toggle('display');
+    Input.classList.toggle('display');
 });
 
 //save todo for later use
